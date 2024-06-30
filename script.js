f5a4bd9cbbe4640a2f00f15c5905a47fd409a48e
BelenDuarte <152980921+BelenDuarte@users.noreply.github.com>
2024-06-29 20:36:41 -0400

    Add files via upload
    Mi segundo proyecto

---
+++ script.js
@@ -0,0 +1,52 @@
+const words = ["TRAIN", "APPLE", "GRAPE", "LEMON", "MANGO", "BERRY", "PEACH"];
+let palabra = words[Math.floor(Math.random() * words.length)];
+let intentos = 6;
+
+const grid = document.getElementById("grid");
+const boton = document.getElementById("guess-button");
+
+boton.addEventListener("click", () => {
+    const intento = leerIntento();
+    console.clear();
+
+    const row = document.createElement("div");
+    row.className = "row";
+
+    for (const i in palabra) {
+        const span = document.createElement("span");
+        span.className = "letter";
+        span.innerHTML = intento[i];
+        if (palabra[i] == intento[i]) { 
+            span.style.backgroundColor = "green";
+        } else if (palabra.includes(intento[i])){
+            span.style.backgroundColor = "yellow";
+        } else {
+            span.style.backgroundColor = "gray";
+        }
+        row.appendChild(span);
+    }
+    grid.appendChild(row);
+
+    if (palabra === intento) {
+        terminar("¡GANASTE! 😀");
+        return;
+    }
+
+    intentos--;
+
+    if (!intentos) {
+        terminar("¡PERDISTE! 😖");
+        return;
+    }
+});
+
+function terminar(mensaje) {
+    const resultado = document.getElementById("guesses");
+    resultado.innerHTML = `<h1>${mensaje}</h1>`;
+    boton.disabled = true;
+}
+
+function leerIntento() {
+    const input = document.getElementById("guess-input");
+    return input.value.toUpperCase();
+}
